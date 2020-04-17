8263a9caef942eeae903c1895baa2e2929d0b529
HibaHarzi <hiba@hannibalmakeapp.com>
2020-04-17 18:24:53 +0200

    initial  commit

---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php
+use Illuminate\Http\Request;
+Route::group([
+    'prefix' => 'auth'
+], function () {
+    Route::post('login', 'Auth\AuthController@login')->name('login');
+    Route::post('register', 'Auth\AuthController@register');
+    Route::group([
+      'middleware' => 'auth:api'
+    ], function() {
+        Route::get('logout', 'Auth\AuthController@logout');
+        Route::get('user', 'Auth\AuthController@user');
+    });
+});
